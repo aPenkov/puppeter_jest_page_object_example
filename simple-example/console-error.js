af3a2399a3c809d13b4e07b227b0ee87e518f7ce
sasha <alexsandr.penkov@drimsim.com>
2019-07-25 23:53:29 +0300

    add a few simple example

--- simple-example/console-error.js
+++ simple-example/console-error.js
@@ -1,13 +1,19 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const bs = await puppeteer.launch({headless:false});
+  const bs = await puppeteer.launch({headless:true});
   const page = await bs.newPage();
+  // page.on('console', msg => {
+  // 	if(msg.type()=='log'){//error
+  // 		console.log('PAGE LOG:', msg.text())
+	//   }
+  // })
 
-  await page.goto('http://localhost:3000/?isOfflineMode&dontWaitForPdf', {waitUntil: 'networkidle2'});
-  page.on("pageerror", function(err) {  
-    theTempValue = err.toString();
-    console.log("Page error: " + theTempValue); 
-    })
+  await page.goto('https://dev3.pdffiller.com/454879723--w9-2018-2019-form-', {waitUntil: 'networkidle2'});
+  await page.click('.fp-form-c2a__submit-btn');
+  await page.waitForXPath(`//ul[@class = 'collapsed-right-sidebar__list']//span[contains(text(), "ADD FILLABLE FIELDS")]`)
+  const cookies = await page.cookies();
+  console.log(cookies)
+  console.log(page.url())
   await bs.close();
 })();
