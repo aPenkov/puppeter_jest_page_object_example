fafcfa46c199fa9d0638c56fec15ae27ee86f7ba
aPenkov <44709430+aPenkov@users.noreply.github.com>
2019-07-26 09:58:19 +0300

    comment get cookie

--- simple-example/console-error.js
+++ simple-example/console-error.js
@@ -1,19 +1,21 @@
 const puppeteer = require('puppeteer');
 
+const url;
+
 (async () => {
   const bs = await puppeteer.launch({headless:true});
   const page = await bs.newPage();
-  // page.on('console', msg => {
-  // 	if(msg.type()=='log'){//error
-  // 		console.log('PAGE LOG:', msg.text())
-	//   }
-  // })
+  page.on('console', msg => {
+  	if(msg.type()=='log'){//error
+  		console.log('PAGE LOG:', msg.text())
+	  }
+  })
 
-  await page.goto('https://dev3.pdffiller.com/454879723--w9-2018-2019-form-', {waitUntil: 'networkidle2'});
+  await page.goto(url, {waitUntil: 'networkidle2'});
   await page.click('.fp-form-c2a__submit-btn');
   await page.waitForXPath(`//ul[@class = 'collapsed-right-sidebar__list']//span[contains(text(), "ADD FILLABLE FIELDS")]`)
-  const cookies = await page.cookies();
-  console.log(cookies)
-  console.log(page.url())
+  //const cookies = await page.cookies();
+  //console.log(cookies)
+  //console.log(page.url())
   await bs.close();
 })();
